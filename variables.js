0daae64a0e0141e4e39949f64233de9c5171a2c9
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-07-10 20:59:40 -0500

    Add sentence

--- variables.js
+++ variables.js
@@ -4,3 +4,8 @@ var age = 28
 var year = 2022
 var enrolled = true
 var depositPaid = true
+
+//This will show an example of string interpolation.
+
+var sentence = `My favorite vegetable is ${veggies)`;
+console.log(`sentence`);
