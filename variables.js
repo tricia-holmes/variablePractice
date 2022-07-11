351e328feba4d40895ad02e5716d576e0f2a97f7
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-07-10 21:19:10 -0500

    Add reassigned values and fixed code

--- variables.js
+++ variables.js
@@ -1,11 +1,21 @@
-var fruit = "apple"
-var veggies = "brussel sprouts"
-var age = 28
-var year = 2022
-var enrolled = true
-var depositPaid = true
+var fruit = "apple";
+var veggies = "brussel sprouts";
+var age = 28;
+var year = 2022;
+var enrolled = true;
+var depositPaid = true;
 
-//This will show an example of string interpolation.
+//The sentence will print "My favorite vegetable is brussel sprouts";
 
-var sentence = `My favorite vegetable is ${veggies)`;
-console.log(`sentence`);
+var sentence = `My favorite vegetable is ${veggies}`;
+console.log (sentence);
+
+// With the reassigned values, console log should now print "kale" and "pineapple"
+var veggies = "kale";
+var fruit = "pineapple";
+console.log(fruit);
+console.log(veggies);
+
+// This is what the string interpolation prints after changing the value of "veggies"
+var sentence = `My favorite vegetable is ${veggies}`;
+console.log (sentence);
